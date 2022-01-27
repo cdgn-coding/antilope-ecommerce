78004f4881aadc4b2508ab410b7cae48d89fb9c2
Carlos David Gonzalez Nexans <cdgn.machines@gmail.com>
2022-01-27 08:52:21 -0300

    Add s3 client

--- ecommerce-backend/src/clients/s3.go
+++ ecommerce-backend/src/clients/s3.go
@@ -1,23 +1,24 @@
 package clients
 
 import (
+	"io"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/guregu/dynamo"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func GetDynamoDBClient() *dynamo.DB {
-	const localEndpoint = "http://localhost:8000"
+func getS3Client(bucketName, key string, rs io.ReadSeeker) *s3.S3 {
+	const localEndpoint = "http://localstack:4566"
 
 	if os.Getenv("env") == "local" {
 		config := aws.Config{Endpoint: aws.String(localEndpoint), Region: aws.String("us-east-1")}
 		session := session.Must(session.NewSession())
-		return dynamo.New(session, &config)
+		return s3.New(session, &config)
 	}
 
 	options := session.Options{SharedConfigState: session.SharedConfigEnable}
 	session := session.Must(session.NewSessionWithOptions(options))
-	return dynamo.New(session)
+	return s3.New(session)
 }
