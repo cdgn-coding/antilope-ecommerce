d34eb6eaaef8f8fc398f0ba61324ca35a351a091
Carlos David Gonzalez Nexans <cdgn.machines@gmail.com>
2022-02-11 09:18:34 -0300

    Fix storybook for Layout

--- frontend/src/components/Layout/Layout.stories.tsx
+++ frontend/src/components/Layout/Layout.stories.tsx
@@ -1,8 +1,20 @@
 import Layout, { LayoutProps } from "./Layout";
+import secondaryMenuItems from "@constants/secondaryMenu";
+import menuItems from "@constants/menuItems";
 
 export default {
   title: "Layout",
   component: Layout,
+  argTypes: {
+    menuItems: {
+      type: "array",
+      defaultValue: menuItems,
+    },
+    secondaryMenuItems: {
+      type: "array",
+      defaultValue: secondaryMenuItems,
+    },
+  },
 };
 
 export const Default = (args: LayoutProps) => {
