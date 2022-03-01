d5fa60b5f1289f24d47f624ef97199ebc845263e
Carlos David Gonzalez Nexans <cdgn.machines@gmail.com>
2022-03-01 08:33:27 -0300

    Export ecr from infra pulumi project

--- infraestructure/index.ts
+++ infraestructure/index.ts
@@ -1,3 +1,9 @@
 export { eksCluster, vpc, kubeconfig } from "./k8";
 
-export { productsBucketId, cartTableId, dbConnection } from "./backend";
+export {
+  productsBucketId,
+  cartTableId,
+  dbConnection,
+  containerRepository as backendRepository,
+} from "./backend";
+export { containerRepository as frontendRepository } from "./frontend";
